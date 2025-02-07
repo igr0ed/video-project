35bfa5487379006343e27a62006219b9e7ff5081
masterWeb2000 <24385105+igr0ed@users.noreply.github.com>
2025-02-07 18:02:34 +0500

    everything is working

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 "use strict"
 
 let btn = document.querySelector('.switch-btn');
+let video = document.querySelector('video');
 
 btn.addEventListener('click', function(event) {
     event.target.classList.toggle('switch-on')
+    if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
 })
